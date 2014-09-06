ac18ae9f79250c7249f8e1012448bfa7afd465d2
bryan-qiu <bryan_qiu@hotmail.com>
2014-09-06 15:27:18 -0400

    sidebar for leap

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-var controller = Leap.loop({enableGestures: true}, function(frame){
-  if (frame.valid && frame.gestures.length > 0){
-    frame.gestures.forEach(function(gesture){
-      if (gesture.type != 'swipe') return;
-      var isVertical = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
-      if(isVertical){
-
-        if(Math.abs(gesture.direction[0])<0.75)return;
-
-        if(gesture.direction[0]>0){
-          console.log('left');
-        } else {
-          console.log('right');
-        }
-      }
-    });
-  }
-});
-
-
 function loadjscssfile(filename, filetype){
  if (filetype=="js"){ //if filename is a external JavaScript file
   var fileref=document.createElement('script')
@@ -34,6 +14,40 @@ function loadjscssfile(filename, filetype){
   document.getElementsByTagName("head")[0].appendChild(fileref)
 }
 
+
+var controller = Leap.loop({enableGestures: true}, function(frame){
+  if (frame.valid && frame.gestures.length > 0){
+    frame.gestures.forEach(function(gesture){
+      if (gesture.type != 'swipe') return;
+      var isVertical = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
+      if(isVertical){
+
+        if(Math.abs(gesture.direction[0])<0.75)return;
+
+        if(gesture.direction[0]>0){
+          console.log('left');
+          $.get(chrome.extension.getURL("prototype/sidebar.html"), function(data){
+
+            $('#test').html(data);
+
+            loadjscssfile(chrome.extension.getURL("prototype/css/bootstrap.css"), "css");
+            loadjscssfile(chrome.extension.getURL("prototype/css/bootstrap-reset.css"), "css");
+            loadjscssfile(chrome.extension.getURL("prototype/assets/font-awesome/css/font-awesome.css"), "css");
+            loadjscssfile(chrome.extension.getURL("prototype/css/navbar-fixed-top.css"), "css");
+            loadjscssfile(chrome.extension.getURL("prototype/css/style.css"), "css");
+            loadjscssfile(chrome.extension.getURL("prototype/css/style-responsive.css"), "css");
+
+            loadjscssfile(chrome.extension.getURL("prototype/js/jquery.js"), "js");
+            loadjscssfile(chrome.extension.getURL("prototype/js/bootstrap.min.js"), "js");
+          });
+        } else {
+          console.log('right');
+        }
+      }
+    });
+  }
+});
+
 document.onclick = function() {
   console.log("hi");
 
@@ -48,7 +62,7 @@ document.onclick = function() {
     loadjscssfile(chrome.extension.getURL("prototype/css/navbar-fixed-top.css"), "css");
     loadjscssfile(chrome.extension.getURL("prototype/css/style.css"), "css");
     loadjscssfile(chrome.extension.getURL("prototype/css/style-responsive.css"), "css");
-    
+
     loadjscssfile(chrome.extension.getURL("prototype/js/jquery.js"), "js");
     loadjscssfile(chrome.extension.getURL("prototype/js/bootstrap.min.js"), "js");
   });
