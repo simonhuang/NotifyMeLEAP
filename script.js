9f9b18be7fe45124ab102db013592f3b1f262194
Michael <michaelyoung1995@gmail.com>
2014-09-06 00:33:15 -0400

    asdada

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ var controller = Leap.loop({enableGestures: true}, function(frame){
   if(frame.valid && frame.gestures.length > 0){
     frame.gestures.forEach(function(gesture){
       if(gesture.type != 'swipe')return;
-      var isVertical = Math.abs(gesture.direction[0]) < Math.abs(gesture.direction[1]);
+      var isVertical = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
       if(isVertical){
 
-        if(Math.abs(gesture.direction[1])<0.75)return;
+        if(Math.abs(gesture.direction[0])<0.75)return;
 
-        if(gesture.direction[1]>0){
-          console.log('up');
+        if(gesture.direction[0]>0){
+          console.log('left');
         }else{
-          console.log('down')
+          console.log('right')
         }
       }
     });
