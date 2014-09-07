6ddce61b2b859295effb9c113b52c9a6e7d2b6dc
PeterGok <petergok@hotmail.com>
2014-09-06 23:26:10 -0400

    Added real weather data

--- prototype/js/google-gmail.js
+++ prototype/js/google-gmail.js
@@ -1,10 +1,6 @@
 function getNews(){
    $.get("http://content.guardianapis.com/search", function (data, status) {
-
-
    	var newsfeed = $('#news-feed');
-   	console.log(newsfeed.length);
-   	console.log (data.response.results.length);
 	var newsString = 
 	            '<section id="accord10" class="panel panel-default">' + 
 	                '<header class="panel-heading">' + 
@@ -28,3 +24,26 @@ function getNews(){
 	newsfeed.html(newsString);
    });
  }
+
+function getWeather() {
+	$.get("http://api.wunderground.com/api/146f20e12bdac7ca/conditions/q/Ann_Arbor.json", function (data, status) {
+		console.log(data);
+		var weatherfeed = $('#weather-data');
+		var result = data.current_observation;
+		var weatherString = '<h4>' + result.observation_time_rfc822.substring(0, 16) + '</h4>' +
+                        '<div class="col-lg-2">' +
+                            '<img src=' + result.icon_url + '>' + 
+                        '</div>' +
+                        '<div class="col-lg-3">' +
+                        '<b>' + result.weather + '<b>' +
+                        '<br>' +
+                        result.temp_f + '°F' +
+                        '<br>' +
+                        '</div>' +
+                        
+                        '<i>Ann Arbour, Michigan</i>';
+        weatherfeed.html(weatherString);
+
+	})
+}
+ 
